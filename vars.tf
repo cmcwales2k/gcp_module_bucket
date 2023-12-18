4f2ae6d32f0a0aca416ebf949e25d43a552fb8cf
cmcwales2k <craigcummings323@gmail.com>
2023-12-18 13:16:22 +0000

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,1 +1,23 @@
+variable "project" {}
 
+variable "credential_file" {}
+
+variable "region" {
+  default = "europe-west2"
+}
+
+variable "zone" {
+  default = "europe-west2-a"
+}
+
+variable "bucket_public_access_prevention" {
+  description = "Prevents public access to a bucket. Acceptable values are inherited or enforced. If inherited, the bucket uses public access prevention, only if the bucket is subject to the public access prevention organization policy constraint."
+  type        = string
+  default     = "inherited"
+}
+
+variable "bucket_force_destroy" {
+  description = "Force destroy bucket"
+  type        = bool
+  default     = true
+}
